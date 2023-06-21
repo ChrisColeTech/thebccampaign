2ec4aaa56ae9c9e9622674d5f9cfe31faa3b2010
ChrisColeTech <moneymagnet216@gmail.com>
2023-06-21 07:45:04 -0400

    Update add-comment.js

--- netlify/functions/add-comment.js
+++ netlify/functions/add-comment.js
@@ -1,33 +1,39 @@
-const { Client, query } = require('faunadb');
+// Function to handle form submission
+const handleFormSubmit = (event) => {
+  event.preventDefault();
 
-/* Configure faunaDB Client with our secret */
-const client = new Client({ secret: 'fnAFG-Ky5LAATX9wNckFUbX0ngbxY2jv_PlqSUVN' });
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const comment = document.getElementById('comment').value;
 
-/* Export our lambda function as named "handler" export */
-const handler = async (event) => {
-  /* Parse the string body into a usable JS object */
-  const data = JSON.parse(event.body);
-  console.log('Function `create` invoked', data);
-  const { name, email, comment } = data;
-  const item = { data: { name, email, comment, timestamp: new Date().toISOString() } };
+  const formData = { name, email, comment };
 
-  /* Construct the fauna query */
-  try {
-    const response = await client.query(query.Create(query.Collection('comments'), item));
-    console.log('Success', response);
-    /* Success! Return the response with statusCode 200 */
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response),
-    };
-  } catch (error) {
-    console.log('Error', error);
-    /* Error! Return the error with statusCode 400 */
-    return {
-      statusCode: 400,
-      body: JSON.stringify(error),
-    };
-  }
+  fetch('/.netlify/functions/create-comment', {
+    method: 'POST',
+    body: JSON.stringify(formData)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Comment added:', data);
+      document.getElementById('commentForm').reset();
+      appendComment(data); // Append the new comment to the comments div
+    })
+    .catch(error => console.error('Error adding comment:', error));
 };
 
-module.exports = { handler };
+// Function to append a single comment to the comments div
+const appendComment = (comment) => {
+  const commentHtml = `
+      <div>
+          <h3>${comment.name}</h3>
+          <p>${comment.comment}</p>
+      </div>
+  `;
+
+  // Append the new comment to the comments div
+  const commentsDiv = document.getElementById('comments');
+  commentsDiv.insertAdjacentHTML('beforeend', commentHtml);
+};
+
+// Attach event listener to form submission
+document.getElementById('commentForm').addEventListener('submit', handleFormSubmit);
