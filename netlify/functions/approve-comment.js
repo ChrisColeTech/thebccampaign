1748c9cc59752d482d22512c645ff8544e3ca670
ChrisColeTech <moneymagnet216@gmail.com>
2023-06-26 02:40:30 -0400

    Update approve-comment.js

--- netlify/functions/approve-comment.js
+++ netlify/functions/approve-comment.js
@@ -1,33 +1,39 @@
-/* Import faunaDB sdk */
-const process = require('process')
-
-const { Client, query } = require('faunadb')
+const { Client, query } = require('faunadb');
 
 /* Configure faunaDB Client with our secret */
 const client = new Client({ secret: 'fnAFG-Ky5LAATX9wNckFUbX0ngbxY2jv_PlqSUVN' });
 
 const handler = async (event) => {
-    const { id } = event
-    console.log(`Function 'update' invoked. Update id: ${id}`)
+    const data = JSON.parse(event.body);
+    const { id } = event;
+    console.log(`Function 'update' invoked. update id: ${id}`);
     try {
         const response = await client.query(
             query.Update(
-                query.Ref(query.Collection('comments'), id),
-                { data: { approved: true } }
+                query.Ref(query.Collection('comments'), commentId),
+                {
+                    data: {
+                        approved: true, // Set the 'approved' field to 'true'
+                    },
+                }
             )
         );
-        console.log('Success:', response);
+        console.log('success', response);
         return {
             statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+            },
             body: JSON.stringify(response),
         };
     } catch (error) {
-        console.error('Error updating comment:', error);
+        console.log('error', error);
         return {
             statusCode: 400,
             body: JSON.stringify(error),
         };
     }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
